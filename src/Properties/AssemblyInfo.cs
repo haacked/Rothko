302e9cecbe465769a451035a4d5f572703d98376
Haacked <haacked@gmail.com>
2012-06-14 16:10:12 -0700

    Initial commit of the solution
    This sets up the following:
    
    * Package Restore
    * R# settings files
    * Unit/Integration test project which references the library.

---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("Rothko")]
+[assembly: AssemblyDescription("A library that provides a testable way to interact with the file system, registry, etc.")]
+[assembly: AssemblyCompany("Phil Haack")]
+[assembly: AssemblyProduct("Rothko")]
+[assembly: AssemblyCopyright("Copyright © Phil Haack 2012")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyInformationalVersion("0.0.1-alpha")]
