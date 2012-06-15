36deff6dd818bb9fbf13022b54df80dd9827cc8e
Haacked <haacked@gmail.com>
2012-06-15 15:37:43 -0700

    Implemented IEnvironment interface
    Abstraction for the System.Environment

---
+++ src/IEnvironment.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Rothko
+{
+    public interface IEnvironment
+    {
+        string CommandLine { get; }
+
+        string CurrentDirectory { get; set; }
+
+        int ExitCode { get; set; }
+
+        bool HasShutdownStarted { get; }
+
+        bool Is64BitOperatingSystem { get; }
+
+        bool Is64BitProcess { get; }
+
+        string MachineName { get; }
+
+        string NewLine { get; }
+
+        IOperatingSystem OSVersion { get; }
+
+        int ProcessorCount { get; }
+
+        string StackTrace { get; }
+
+        string SystemDirectory { get; }
+
+        int SystemPageSize { get; }
+
+        int TickCount { get; }
+
+        string UserDomainName { get; }
+
+        bool UserInteractive { get; }
+
+        string UserName { get; }
+
+        Version Version { get; }
+
+        long WorkingSet { get; }
+
+        void Exit(int exitCode);
+
+        string ExpandEnvironmentVariables(string name);
+
+        void FailFast(string message);
+
+        void FailFast(string message, Exception exception);
+
+        string[] GetCommandLineArgs();
+
+        string GetEnvironmentVariable(string variable);
+
+        string GetEnvironmentVariable(string variable, EnvironmentVariableTarget target);
+
+        IDictionary<string, string> GetEnvironmentVariables();
+
+        IDictionary<string, string> GetEnvironmentVariables(EnvironmentVariableTarget target);
+
+        string GetFolderPath(Environment.SpecialFolder folder);
+
+        string GetFolderPath(Environment.SpecialFolder folder, Environment.SpecialFolderOption option);
+
+        string[] GetLogicalDrives();
+
+        void SetEnvironmentVariable(string variable, string value);
+
+        void SetEnvironmentVariable(string variable, string value, EnvironmentVariableTarget target);
+    }
+}
---
+++ src/IOperatingSystem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Rothko
+{
+    public interface IOperatingSystem : ICloneable, ISerializable
+    {
+        PlatformID Platform { get; }
+        string ServicePack { get; }
+        Version Version { get; }
+        string VersionString { get; }
+    }
+}
