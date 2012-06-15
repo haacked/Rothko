9f1b401d184c9cefd96510461a34411e232cc966
Haacked <haacked@gmail.com>
2012-06-15 16:14:28 -0700

    Implemented IDirectoryInfo interface

---
+++ src/IDirectoryInfo.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Security.AccessControl;
+
+namespace Rothko
+{
+    public interface IDirectoryInfo
+    {
+        IDirectoryInfo Parent { get; }
+
+        IDirectoryInfo Root { get; }
+
+        void Create();
+
+        void Create(DirectorySecurity directorySecurity);
+
+        IDirectoryInfo CreateSubdirectory(string path);
+
+        IDirectoryInfo CreateSubdirectory(string path, DirectorySecurity directorySecurity);
+
+        void Delete(bool recursive);
+
+        IEnumerable<IDirectoryInfo> EnumerateDirectories();
+
+        IEnumerable<IDirectoryInfo> EnumerateDirectories(string searchPattern);
+
+        IEnumerable<IDirectoryInfo> EnumerateDirectories(string searchPattern, SearchOption searchOption);
+
+        IEnumerable<IFileInfo> EnumerateFiles();
+
+        IEnumerable<IFileInfo> EnumerateFiles(string searchPattern);
+
+        IEnumerable<IFileInfo> EnumerateFiles(string searchPattern, SearchOption searchOption);
+
+        IEnumerable<IFileSystemInfo> EnumerateFileSystemInfos();
+
+        IEnumerable<IFileSystemInfo> EnumerateFileSystemInfos(string searchPattern);
+
+        IEnumerable<IFileSystemInfo> EnumerateFileSystemInfos(string searchPattern, SearchOption searchOption);
+
+        DirectorySecurity GetAccessControl();
+
+        DirectorySecurity GetAccessControl(AccessControlSections includeSections);
+
+        void MoveTo(string destDirName);
+
+        void SetAccessControl(DirectorySecurity directorySecurity);
+    }
+}
