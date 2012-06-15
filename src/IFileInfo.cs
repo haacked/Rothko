6e5cb5458842922710e170e350c32703ae78c1f3
Haacked <haacked@gmail.com>
2012-06-15 16:09:47 -0700

    Implemented IFileInfo interface

---
+++ src/IFileInfo.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using System.Security.AccessControl;
+
+namespace Rothko
+{
+    public interface IFileInfo : IFileSystemInfo
+    {
+        DirectoryInfo Directory { get; }
+
+        string DirectoryName { get; }
+
+        bool IsReadOnly { get; set; }
+
+        long Length { get; }
+
+        StreamWriter AppendText();
+
+        FileInfo CopyTo(string destFileName);
+
+        FileInfo CopyTo(string destFileName, bool overwrite);
+
+        FileStream Create();
+
+        StreamWriter CreateText();
+
+        void Decrypt();
+
+        void Encrypt();
+
+        FileSecurity GetAccessControl();
+
+        FileSecurity GetAccessControl(AccessControlSections includeSections);
+
+        void MoveTo(string destFileName);
+
+        FileStream Open(FileMode mode);
+
+        FileStream Open(FileMode mode, FileAccess access);
+
+        FileStream Open(FileMode mode, FileAccess access, FileShare share);
+
+        FileStream OpenRead();
+
+        StreamReader OpenText();
+
+        FileStream OpenWrite();
+
+        FileInfo Replace(string destinationFileName, string destinationBackupFileName);
+
+        FileInfo Replace(string destinationFileName, string destinationBackupFileName, bool ignoreMetadataErrors);
+
+        void SetAccessControl(FileSecurity fileSecurity);
+    }
+}
