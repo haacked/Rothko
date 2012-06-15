ae040e66d2a33c0e5322459ad46629c0f94cefd5
Haacked <haacked@gmail.com>
2012-06-15 15:43:26 -0700

    Added IRegistry interface
    Abstraction for the registry. Need to abstract RegistryKey.

---
+++ src/IRegistry.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Win32;
+
+namespace Rothko
+{
+    public interface IRegistry
+    {
+        RegistryKey ClassesRoot { get; }
+
+        RegistryKey CurrentConfig { get; }
+
+        RegistryKey CurrentUser { get; }
+
+        RegistryKey LocalMachine { get; }
+
+        RegistryKey PerformanceData { get; }
+
+        RegistryKey Users { get; }
+
+        object GetValue(string keyName, string valueName, object defaultValue);
+
+        void SetValue(string keyName, string valueName, object value);
+
+        void SetValue(string keyName, string valueName, object value, RegistryValueKind valueKind);
+    }
+}
