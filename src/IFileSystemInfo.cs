c74ac2f9b50ac77356e47c96fade942214e71df5
Haacked <haacked@gmail.com>
2012-06-15 15:57:14 -0700

    Implemented IFileSystemInfo interface

---
+++ src/IFileSystemInfo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+
+namespace Rothko
+{
+    public interface IFileSystemInfo : ISerializable
+    {
+        FileAttributes Attributes { get; set; }
+
+        DateTime CreationTime { get; set; }
+
+        DateTime CreationTimeUtc { get; set; }
+
+        bool Exists { get; }
+
+        string Extension { get; }
+
+        string FullName { get; }
+
+        DateTime LastAccessTime { get; set; }
+
+        DateTime LastAccessTimeUtc { get; set; }
+
+        DateTime LastWriteTime { get; set; }
+
+        DateTime LastWriteTimeUtc { get; set; }
+
+        string Name { get; }
+
+        void Delete();
+
+        void Refresh();
+    }
+}
