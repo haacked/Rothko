9b40c731c4fbf1ae93a8f29bfae7619e65d35020
Haacked <haacked@gmail.com>
2013-12-11 13:38:07 -0800

    Add NullGuard.Fody
    This validates null arguments for us.

---
+++ Tests/NullGuardTest.cs
@@ -0,0 +1,19 @@
+﻿using Rothko;
+using Xunit;
+using Xunit.Sdk;
+
+namespace Tests
+{
+    public class NullGuardTest
+    {
+        [Fact]
+        public void MakeSureNullGuardIsWorking()
+        {
+#if DEBUG
+            Assert.Throws<TraceAssertException>(() => new FileInfo(null));
+#else
+            Assert.Throws<ArgumentNullException>(() => new FileInfo(null));
+#endif
+        }
+    }
+}
