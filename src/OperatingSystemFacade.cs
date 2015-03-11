0a56be6a2f5c693f695e242ec5d416ce79733998
Haacked <haacked@gmail.com>
2015-03-10 20:38:03 -0700

    Add methods for launching a browser to a URL

--- src/OperatingSystemFacade.cs
+++ src/OperatingSystemFacade.cs
@@ -1,4 +1,6 @@
-﻿namespace Rothko
+﻿using Rothko.Infrastructure;
+
+namespace Rothko
 {
     public class OperatingSystemFacade : IOperatingSystem
     {
@@ -13,6 +15,7 @@
             ProcessLocator = new ProcessLocator();
             ProcessStarter = new ProcessStarter();
             Registry = new Registry();
+            Browser = new Browser(ProcessStarter, Environment);
         }
 
         public IAssemblyFacade Assembly { get; private set; }
@@ -24,5 +27,7 @@
         public IProcessLocator ProcessLocator { get; private set; }
         public IProcessStarter ProcessStarter { get; private set; }
         public IRegistry Registry { get; private set; }
+
+        public IBrowser Browser { get; private set; }
     }
 }
