63c3f8b0e4a1a9851b5790ceee009e8e41e353cb
Haacked <haacked@gmail.com>
2015-03-16 16:38:38 -0700

    Add useful methods to Rothko for file operations
    I'm surprised I hadn't already implemented more!

--- src/Infrastructure/FileFacade.cs
+++ src/Infrastructure/FileFacade.cs
@@ -1,12 +1,44 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
 namespace Rothko
 {
     public class FileFacade : IFileFacade
     {
+        public bool Exists(string path)
+        {
+            return File.Exists(path);
+        }
+
+        public IFileInfo GetFile(string path)
+        {
+            return FileInfo.Wrap(new System.IO.FileInfo(path));
+        }
+
         public void WriteAllText(string path, string contents)
         {
             File.WriteAllText(path, contents);
+        }
+
+        public void Copy(string sourceFileName, string destFileName, bool overwrite)
+        {
+            File.Copy(sourceFileName, destFileName, overwrite);
+        }
+
+        public void Move(string sourceFileName, string destFileName)
+        {
+            File.Move(sourceFileName, destFileName);
+        }
+
+        public IEnumerable<string> ReadLines(string path, Encoding encoding)
+        {
+            return File.ReadLines(path, encoding);
+        }
+
+        public string ReadAllText(string path, Encoding encoding)
+        {
+            return File.ReadAllText(path, encoding);
         }
     }
 }
