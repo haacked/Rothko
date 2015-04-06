d8df92827f64c5f60e345f39947bb58dedef895a
Peter MacNaughton <pmacnaughton@energylab.com>
2015-04-06 09:50:52 -0600

    Add ShowOpenFileDialog
    Add simple abstraction for OpenFileDialog on IDialogFacade

--- src/Infrastructure/IDialogFacade.cs
+++ src/Infrastructure/IDialogFacade.cs
@@ -12,6 +12,14 @@ namespace Rothko
         SaveDialogResult ShowSaveFileDialog(string filterPattern);
 
         /// <summary>
+        /// Displays an open file dialog and returns the result of the user's interaction with the dialog.
+        /// </summary>
+        /// <param name="filePattern">Pattern used to filter the files shown.</param>
+        /// <param name="multiselect">Indicates whether the dialog box allows multiple files to be selected.</param>
+        /// <returns>A <see cref="OpenDialogResult"/> indicating the action taken by the user.</returns>
+        OpenDialogResult ShowOpenFileDialog(string filePattern, bool multiselect);
+
+        /// <summary>
         /// Opens a standard Windows browse directory and returns the result.
         /// </summary>
         /// <param name="selectedPath">The default selected path</param>
