1abd7773cc95762842b21bec3a5356afed921432
Haacked <haacked@gmail.com>
2015-12-10 20:15:23 -0800

    Add some useful helper classes for testing

---
+++ Tests/DirectoryInfoTests.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using Xunit;
+using DirectoryInfo = Rothko.DirectoryInfo;
+
+public class DirectoryInfoTests
+{
+    public class TheCreateMethod
+    {
+        [Fact]
+        public void CreatesMissingParentDirectories()
+        {
+            using (var tempDir = DisposableDirectory.CreateRandomDirectory())
+            {
+                var path = Path.Combine(tempDir.FullPath, "foo", "bar", "baz");
+                var directory = new DirectoryInfo(path);
+                Assert.False(Directory.Exists(Path.Combine(tempDir.FullPath, "foo")));
+                Assert.False(Directory.Exists(Path.Combine(tempDir.FullPath, "foo", "bar")));
+                Assert.False(Directory.Exists(path));
+
+                directory.Create();
+
+                Assert.True(Directory.Exists(Path.Combine(tempDir.FullPath, "foo")));
+                Assert.True(Directory.Exists(Path.Combine(tempDir.FullPath, "foo", "bar")));
+                Assert.True(Directory.Exists(path));
+            }
+        }
+    }
+}
---
+++ Tests/Helpers/DisposableDirectory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+public sealed class DisposableDirectory : IDisposable
+{
+    readonly DirectoryInfo directory;
+
+    public static DisposableDirectory CreateRandomDirectory()
+    {
+        return CreateDirectory(GetRandomRothkoTemporaryFolder());
+    }
+
+    public static DisposableDirectory CreateDirectory(string path)
+    {
+        return new DisposableDirectory(new DirectoryInfo(path));
+    }
+
+    DisposableDirectory(DirectoryInfo directory)
+    {
+        this.directory = directory;
+    }
+
+    public string FullPath { get { return directory.FullName; } }
+
+    public void Dispose()
+    {
+        directory.Delete(true);
+    }
+
+    static string GetRandomRothkoTemporaryFolder()
+    {
+        return Path.Combine(Path.GetTempPath(), "__RothkoTestFolder-REMOVE-ME", Path.GetRandomFileName());
+    }
+}
