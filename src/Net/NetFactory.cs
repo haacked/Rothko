a2d9b573f7a465e83c2624a3849d893bfb24900f
Steven Kirk <grokys@gmail.com>
2017-11-15 11:18:54 +0100

    Added wrapper for HttpListener.

---
+++ src/Net/NetFactory.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace Rothko
+{
+    public class NetFactory : INetFactory
+    {
+        public IHttpListener CreateHttpListener()
+        {
+            return new HttpListenerWrapper(new HttpListener());
+        }
+    }
+}
